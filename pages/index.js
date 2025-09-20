5c4dd9cece679be428a0fa5c31848a6162557ce7
livii7207-dotcom <livii7207@gmail.com>
2025-09-20 08:10:06 -0500

    update: gradient background for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
+import LandingPage from "../components/LandingPage";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to Cammodelsdaily!</h1>
-      <p>Your Next.js app is deployed and running on Vercel.</p>
-    </main>
-  );
-}
+  return <LandingPage />;
+  }
